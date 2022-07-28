c559fba490ad734a2cb757f8a84d7884fba72761
Mendd <56957245+MenddGabriel@users.noreply.github.com>
2022-07-28 16:14:47 -0300

    beecrowd | 1016 - Distância
    https://www.beecrowd.com.br/judge/pt/problems/view/1016

---
+++ uri1016/uri1016.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int distancia = 0;
+    int tempo = 0;
+
+    scanf("%d", &distancia);
+
+    tempo = distancia * 2;
+
+    printf("%d minutos\n", tempo);
+
+    return(0);
+}

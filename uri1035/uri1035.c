b662e5a45f988d29632a7edaa28edea86b17383a
Mendd <56957245+MenddGabriel@users.noreply.github.com>
2022-07-29 22:37:31 -0300

    beecrowd | 1035 - Teste de Seleção 1
    https://www.beecrowd.com.br/judge/pt/problems/view/1035

---
+++ uri1035/uri1035.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main (){
+	
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+	
+	
+	
+	if(b > c && d > a && c + d > a + b && c >= 0 && d >= 0 && a % 2 == 0){
+		
+		printf("Valores aceitos\n");
+		
+	}
+	else{
+		
+		printf("Valores nao aceitos\n");
+		
+	}
+	
+	return 0;
+	
+}
